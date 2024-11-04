89953751bbd07af6c3c541a94899f670f9f2a113
Toyoabasi Bob <bobtoyoabasi@gmail.com>
2024-11-04 19:22:12 +0100

    finished navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import HeaderTop from "@/components/HeaderTop";
 import HeaderMain from "@/components/HeaderMain";
+import Navbar from "@/components/Navbar";
 
 
 const inter = Inter({ subsets: ["latin"] })
@@ -23,6 +24,7 @@ export default function RootLayout({
         className={inter.className}>
         <HeaderTop />
         <HeaderMain />
+        <Navbar />
         {children}
       </body>
     </html>
