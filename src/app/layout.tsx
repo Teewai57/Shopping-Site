f796765bf4678f31e54c82983735e0718fe7d466
Toyoabasi Bob <bobtoyoabasi@gmail.com>
2024-11-04 18:08:23 +0100

    created first section of navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import HeaderTop from "@/components/HeaderTop";
+import HeaderMain from "@/components/HeaderMain";
 
 
 const inter = Inter({ subsets: ["latin"] })
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body
         className={inter.className}>
         <HeaderTop />
+        <HeaderMain />
         {children}
       </body>
     </html>
